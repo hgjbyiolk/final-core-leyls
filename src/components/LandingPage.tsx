13e271257a19a6d1dd60f8c99413a45d15de1ddb
hgjbyiolk <hgjbyiolk@yahoo.com>
2025-10-06 14:46:38 +0800

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -176,7 +176,7 @@ const LandingPage: React.FC = () => {
               <img 
         src="/leyls-svg.svg" 
         alt="Leyls" 
-        className="h-12 sm:h-16 md:h-20 w-auto object-contain" 
+        className="h-14 sm:h-18 md:h-22 w-auto object-contain" 
       />
             </div>
 
