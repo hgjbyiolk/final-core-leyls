0a3d5a8b70a5cfad1c0680295e0f2abe6251d141
hgjbyiolk <hgjbyiolk@yahoo.com>
2025-10-06 16:00:40 +0800

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -171,12 +171,14 @@ const LandingPage: React.FC = () => {
         style={{ opacity: headerOpacity }}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
+          {/* CHANGED: h-16 to h-20 (or h-24 if needed) to give more room for the logo */}
+          <div className="flex items-center justify-between h-20"> 
             <div className="flex items-center gap-3">
               <img 
         src="/leyls-svg.svg" 
         alt="Leyls" 
-        className="h-12 sm:h-16 md:h-20 w-auto object-contain" 
+        {/* CHANGED: Adjusted logo height classes to match the new header height */}
+        className="h-16 sm:h-16 md:h-16 w-auto object-contain" 
       />
             </div>
 
