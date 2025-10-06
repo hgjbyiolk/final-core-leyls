5e6861ab1bbc14914f2f95b4e36285441566d301
hgjbyiolk <hgjbyiolk@yahoo.com>
2025-10-06 14:43:38 +0800

    Updated LandingPage.tsx

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -171,10 +171,18 @@ const LandingPage: React.FC = () => {
         style={{ opacity: headerOpacity }}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex items-center justify-between h-16">
-            <div className="flex items-center gap-3">
-              <img src="/leyls-svg.svg" alt="Leyls" className="h-9 w-auto object-contain" />
-            </div>
+  <div className="flex items-center justify-between h-16">
+    <div className="flex items-center gap-3">
+      <img 
+        src="/leyls-svg.svg" 
+        alt="Leyls" 
+        className="h-12 sm:h-16 md:h-20 w-auto object-contain" 
+      />
+    </div>
+    {/* Other items like nav links or buttons can go here */}
+  </div>
+</div>
+
 
             {/* Desktop Navigation */}
             <nav className="hidden md:flex items-center gap-8">
